71a186e4f9a6a8bed9de851e9aa6ef9e45caf510
jwooj <jwooj@BOOK-CSSJ9EP86S>
2024-08-19 19:12:02 +0900

    v 1

--- regressionFourthOrder.m
+++ regressionFourthOrder.m
@@ -1,10 +1,11 @@
-function output = regressionFourthOrder(inputMatrix)
+function [beta error] = regressionFourthOrder(inputMatrix)
     %inputSensor=inputMatrix + error*rand(length(inputMatrix(:,1)),length(inputMatrix(1,:)))-error*0.5;
     Temp=calculateFourthOrder(inputMatrix);
     
     tic
-    output = inv(Temp.' * Temp)* Temp.' * ones(length(inputMatrix(:,1)),1);
+    beta = inv(Temp.' * Temp)* Temp.' * ones(length(inputMatrix(:,1)),1);
+    error = ones(length(inputMatrix(:,1)),1)- Temp * beta;
+
     toc 
-
-
+    
 end
