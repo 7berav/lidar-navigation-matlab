6a7a1a5e094beb1a89092b1d6a1bf1ab61d8f357
jwooj <jwooj@BOOK-CSSJ9EP86S>
2024-08-20 14:20:02 +0900

    v 0

---
+++ calculateDerivative.m
@@ -0,0 +1,15 @@
+function output = calculateDerivative(inputMatrix)
+    % 입력 행렬에서 각 행을 추출하여 각각의 변수 x, y, z로 지정
+    x = inputMatrix(:, 1);
+    y = inputMatrix(:, 2);
+    z = inputMatrix(:, 3);
+    
+    % 각 항을 계산
+    dx = 4x.^3;
+    dy = 4y.^3;
+    dz = 4z.^3;
+    
+    
+    % 결과 행렬을 생성
+    output = [dx dy dz];
+end
