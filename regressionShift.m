091ff44b7770a216ada706dc1c4126ada64cce9e
jwooj <jwooj@BOOK-CSSJ9EP86S>
2024-08-30 16:56:37 +0900

    V 1.0.2 아무것도 된게 없음

--- regressionShift.m
+++ regressionShift.m
@@ -1,4 +1,4 @@
-function [beta error] = regressionShift(inputMatrix , errorInput, f)
+function [beta error] = regressionShift(inputMatrix , errorInput, f,dx,dy,dz)
     %inputSensor=inputMatrix + error*rand(length(inputMatrix(:,1)),length(inputMatrix(1,:)))-error*0.5;
     
     Temp = calculateDerivative(inputMatrix, f);
--- testcode2.m
+++ testcode2.m
@@ -32,69 +32,53 @@ scatter3(PP6(:,1),PP6(:,2),PP6(:,3),'y');
 hold off
 %}
 
-%% 일반 회귀 후 plot
-%{
-%[beta0 error0]=regressionFourthOrder([PPm])
-[beta1 error1]=regressionFourthOrder([PP2;PP4]);
-[beta2 error2]=regressionFourthOrder([PP4]);
-[beta3 error3]=regressionFourthOrder([PP]);
-[beta4 error4]=regressionFourthOrder([R_PP2]);
-
-f=@(x,y,z) beta2(1)*x.^4+  beta2(2)*y.^4+    beta2(3)*z.^4+    beta2(4)*x.^2*y.^2   +beta2(5)*x.^2*z.^2   +beta2(6)*y.^2*z.^2 ...
-   +beta2(7)*(x.^3).*y    +beta2(8)*(x.^3).*z    +beta2(9)*(y.^3).*x    +beta2(10)*(y.^3).*z    +beta2(11)*(z.^3).*x    +beta2(12)*(z.^3).*y -1 ...
-   +beta2(13)*(x.^2).*y.*z+beta2(14)*(y.^2).*z.*x+beta2(15)*(z.^2).*x.*y;
-g=@(x,y,z) beta1(1)*x.^4+  beta1(2)*y.^4+    beta1(3)*z.^4+    beta1(4)*x.^2*y.^2   +beta1(5)*x.^2*z.^2   +beta1(6)*y.^2*z.^2 ...
-   +beta1(7)*(x.^3).*y    +beta1(8)*(x.^3).*z    +beta1(9)*(y.^3).*x    +beta1(10)*(y.^3).*z    +beta1(11)*(z.^3).*x    +beta1(12)*(z.^3).*y -1 ...
-   +beta1(13)*(x.^2).*y.*z+beta1(14)*(y.^2).*z.*x+beta1(15)*(z.^2).*x.*y;
-
-%h=@(x,y,z) Ans3(1)*x.^4+  Ans3(2)*y.^4+    Ans3(3)*z.^4+    Ans3(4)*x.^2*y.^2   +Ans3(5)*x.^2*z.^2   +Ans3(6)*y.^2*z.^2 ...
-%   +Ans3(7)*(x.^3).*y    +Ans3(8)*(x.^3).*z    +Ans3(9)*(y.^3).*x    +Ans3(10)*(y.^3).*z    +Ans3(11)*(z.^3).*x    +Ans3(12)*(z.^3).*y -1 ;
-
-
-hr=@(x,y,z) beta4(1)*x.^4+ beta4(2)*y.^4+    beta4(3)*z.^4+    beta4(4)*x.^2*y.^2   +beta4(5)*x.^2*z.^2   +beta4(6)*y.^2*z.^2 ...
-   +beta4(7)*(x.^3).*y    +beta4(8)*(x.^3).*z    +beta4(9)*(y.^3).*x    +beta4(10)*(y.^3).*z    +beta3(11)*(z.^3).*x    +beta4(12)*(z.^3).*y -1 ...
-   +beta4(13)*(x.^2).*y.*z+beta4(14)*(y.^2).*z.*x+beta4(15)*(z.^2).*x.*y;
-h=@(x,y,z) beta3(1)*x.^4+  beta3(2)*y.^4+    beta3(3)*z.^4+    beta3(4)*x.^2*y.^2   +beta3(5)*x.^2*z.^2   +beta3(6)*y.^2*z.^2 ...
-   +beta3(7)*(x.^3).*y    +beta3(8)*(x.^3).*z    +beta3(9)*(y.^3).*x    +beta3(10)*(y.^3).*z    +beta3(11)*(z.^3).*x    +beta3(12)*(z.^3).*y -1 ...
-   +beta3(13)*(x.^2).*y.*z+beta3(14)*(y.^2).*z.*x+beta3(15)*(z.^2).*x.*y;
-%}
-%%
-%{
-beta2
-figure;
-hold on
-fimplicit3(g,[-1.5 1.5 -1.5 1.5 -1.5 1.5]);
-scatter3(PP2(:,1),PP2(:,2),PP2(:,3),'b');
-scatter3(PP4(:,1),PP4(:,2),PP4(:,3),'g');
-hold off
-%}
-
 
 %%
-%{
-figure;
-hold on
-%fimplicit3(h,[-1.5 1.5 -1.5 1.5 -1.5 1.5]);
-fimplicit3(hr,[-1.5 1.5 -1.5 1.5 -1.5 1.5]);
-scatter3(PP(:,1),PP(:,2),PP(:,3),'g');
-scatter3(R_PP2(:,1),R_PP2(:,2),R_PP2(:,3),'b');
+syms x;
+syms y;
+syms z;
 
-hold off
+beta = sym('beta', [1, 15]);  % 15개의 심볼릭 변수로 beta 정의
+f2= beta(1)*x.^4 + beta(2)*y.^4 + beta(3)*z.^4 + ...
+    beta(4)*x.^2.*y.^2 + beta(5)*x.^2.*z.^2 + beta(6)*y.^2.*z.^2 + ...
+    beta(7)*(x.^3).*y + beta(8)*(x.^3).*z + beta(9)*(y.^3).*x + ...
+    beta(10)*(y.^3).*z + beta(11)*(z.^3).*x + beta(12)*(z.^3).*y - 1 + ...
+    beta(13)*(x.^2).*y.*z + beta(14)*(y.^2).*z.*x + beta(15)*(z.^2).*x.*y;
+%함수 세팅을 1 뺀꼴로 하면 힘들것 같다. 나중에 형상 피팅할떄 힘들다. 지금은 수동이지만. 
+difx = diff(f2, x);
+dify = diff(f2, y);
+difz = diff(f2, z);
 
-figure;
-hold on
-%fimplicit3(h,[-1.5 1.5 -1.5 1.5 -1.5 1.5]);
-fimplicit3(h_2,[-1.5 1.5 -1.5 1.5 -1.5 1.5]);
-scatter3(PP(:,1),PP(:,2),PP(:,3),'g');
+%0.08s
+f2_numeric = matlabFunction(f2, 'Vars', {[x, y, z], beta});
+d1_numeric = matlabFunction(difx, 'Vars', {[x, y, z], beta});
+d2_numeric = matlabFunction(dify, 'Vars', {[x, y, z], beta});
+d3_numeric = matlabFunction(difz, 'Vars', {[x, y, z], beta});
 
 
-hold off
-%}
+PPm_use = PPS1;
+[beta_values,error]    = regressionFourthOrder(PPm_use);
 
-%%
-PPm_use = PPmm3;
-[beta,error]    = regressionFourthOrder(PPm_use);
-betaError = norm(beta(4:15),1)
+
+%beta 고정
+%0.00026s
+tic
+f2_partial = @(xyz) f2_numeric(xyz, beta_values.');
+toc
+
+%개수따라 다르지만 0.0028~0.005s
+%tic
+%dx_test = d1_partial(PP);
+%toc
+
+%0.003
+tic 
+dx_test = d1_numeric(PP, beta_values.');
+dy_test = d2_numeric(PP, beta_values.');
+dz_test = d3_numeric(PP, beta_values.');
+toc
+
+
 % 초기 값 설정
 PPm_shift = PPm_use;  % 초기 PPm 설정
 error_shift = error;  % 초기 에러 설정
@@ -106,25 +90,13 @@ shiftResidue = shiftReal.';
 shiftResidueSet = [];
 numIterations = 150;  % 반복 횟수 설정
 
-syms x;
-syms y;
-syms z;
-f2= beta(1)*x.^4 + beta(2)*y.^4 + beta(3)*z.^4 + ...
-    beta(4)*x.^2.*y.^2 + beta(5)*x.^2.*z.^2 + beta(6)*y.^2.*z.^2 + ...
-    beta(7)*(x.^3).*y + beta(8)*(x.^3).*z + beta(9)*(y.^3).*x + ...
-    beta(10)*(y.^3).*z + beta(11)*(z.^3).*x + beta(12)*(z.^3).*y - 1 + ...
-    beta(13)*(x.^2).*y.*z + beta(14)*(y.^2).*z.*x + beta(15)*(z.^2).*x.*y;
-%f = @(x, y, z) beta(1)*x.^4 + beta(2)*y.^4 + beta(3)*z.^4 + ...
-%               beta(4)*x.^2.*y.^2 + beta(5)*x.^2.*z.^2 + beta(6)*y.^2.*z.^2 + ...
-%               beta(7)*(x.^3).*y + beta(8)*(x.^3).*z + beta(9)*(y.^3).*x + ...
-%               beta(10)*(y.^3).*z + beta(11)*(z.^3).*x + beta(12)*(z.^3).*y - 1 + ...
-%               beta(13)*(x.^2).*y.*z + beta(14)*(y.^2).*z.*x + beta(15)*(z.^2).*x.*y;
+
+
+%%
 figure
 hold on
-f2_numeric = matlabFunction(f2,'Vars',[x,y,z]);
-fimplicit3(f2_numeric,[-4.5 4.5 -4.5 4.5 -4.5 4.5]);
+fimplicit3(f2_partial,[-4.5 4.5 -4.5 4.5 -4.5 4.5]);
 scatter3(PPm_use(:,1),PPm_use(:,2),PPm_use(:,3),'b');
-
 hold off
 axis equal
 
@@ -139,9 +111,9 @@ axis equal
 for i = 1:numIterations
     % 선형 회귀 및 4차식 피팅
     1
-    
+    tic
     [shift, residual] = regressionShift(PPm_shift, error_shift,f2);
-    
+    toc
     %소요시간 0.12초 내외
     
     shiftSet = [shiftSet shift];
