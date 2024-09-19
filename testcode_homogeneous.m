d2cbc8985780282e527b04cb99d1cc19d0e660c5
jwooj <jwooj@BOOK-CSSJ9EP86S>
2024-09-19 14:19:55 +0900

    H1.2.2

--- testcode_homogeneous.m
+++ testcode_homogeneous.m
@@ -1,16 +1,16 @@
 % Step 1: Define symbolic variables
 syms x y z
-order = 6;
+order = 4;
 
 % Step 2: Create all symbolic terms of order 6
 terms = {};
-for i = 0:order
-    for j = 0:(order-i)
+for i = order:-1:0
+    for j = (order-i):-1:0
         k = order - i - j;
         terms{end+1} = x^i * y^j * z^k; % x^i * y^j * z^k where i+j+k=6
     end
 end
-
+%%
 % Convert the cell array to a symbolic array
 terms = [terms{:}];
 
@@ -18,7 +18,7 @@ terms = [terms{:}];
 beta = sym('beta', [1, length(terms)]); % length(terms) should match with the number of terms
 
 % Step 4: Create the symbolic function as an inner product of beta and terms
-f2 = sum(beta .* terms); % This sums up all beta(i)*term(i)
+f1 = sum(beta .* terms); % This sums up all beta(i)*term(i)
 
 % Display the resulting symbolic function
-disp(f2);
+disp(f1);
