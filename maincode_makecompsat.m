1a3f8a26ef14f9d6da2ebdd697c48a25dbf6e761
jwooj <jwooj@BOOK-CSSJ9EP86S>
2025-03-25 11:05:09 +0900

    V3.2.1

--- maincode_makecompsat.m
+++ maincode_makecompsat.m
@@ -1,65 +1,76 @@
 %2024 08 20
-PP01 = generateRandomPointsOnHexagonPrism(2000)+randn(2000,3)*0.0001;
+PP01 = generateRandomPointsOnHexagonPrism(3000)+randn(3000,3)*0.0007;
 PP02 = generateRandomPointsOnCube(1000)+randn(1000,3)*0.0005;
-PP03 = generateRandomPointsOnCylinder(200)+randn(200,3)*0.0005;
-PP04 = generateRandomPointsOnCube(700);
-shiftReal =  [0.0 -0 -0.01];
+PP03 = generateRandomPointsOnCylinder(70)+randn(70,3)*0.0005;
+PP04 = generateRandomPointsOnCube(200);
+shiftReal =  [0.0 -0 -0.00];
 PPm_body = PP01 ;
 PPm_body(:,1) = PPm_body(:,1) * 0.576;
 PPm_body(:,2) = PPm_body(:,2) * 0.576;
 PPm_body(:,3) = PPm_body(:,3) * 1.165;
-Vector_temp = randn(2000,3)*1;
-Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
-PPm_body = PPm_body + Vector_temp;
-scatter3(PPm_body(:,1),PPm_body(:,2),PPm_body(:,3),2, "filled");
-axis equal
-grid on
+%Vector_temp = randn(size(PPm_body,1),3)*1;
+%Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.3;
+%PPm_body = PPm_body + Vector_temp;
+
 
 PPm_panel1 = PP02 ;
 PPm_panel1(:,1) = PPm_panel1(:,1) / 16;
 PPm_panel1(:,2) = PPm_panel1(:,2) * 1.25;
 PPm_panel1(:,3) = PPm_panel1(:,3) * 0.576;
 PPm_panel1 = PPm_panel1 + [0 2.025 0];
-Vector_temp = randn(1000,3)*1;
-Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
-PPm_panel1 = PPm_panel1 + Vector_temp;
-scatter3(PPm_panel1(:,1),PPm_panel1(:,2),PPm_panel1(:,3),2, "filled");
-axis equal
-grid on
+%Vector_temp = randn(size(PPm_panel1,1),3)*1;
+%Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
+%PPm_panel1 = PPm_panel1 + Vector_temp;
+
 
 PPm_panel2 = PP02 ;
 PPm_panel2(:,1) = PPm_panel2(:,1) / 16;
 PPm_panel2(:,2) = PPm_panel2(:,2) * 1.25;
 PPm_panel2(:,3) = PPm_panel2(:,3) * 0.576;
 PPm_panel2 = PPm_panel2 - [0 2.025 0];
-Vector_temp = randn(1000,3)*1;
-Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
-PPm_panel2 = PPm_panel2 + Vector_temp;
+%Vector_temp = randn(size(PPm_panel2,1),3)*1;
+%Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
+%PPm_panel2 = PPm_panel2 + Vector_temp;
 
 PPm_box1 = PP04 ;
 PPm_box1(:,1) = PPm_box1(:,1) * 0.333;
 PPm_box1(:,2) = PPm_box1(:,2) * 0.175;
 PPm_box1(:,3) = PPm_box1(:,3) * 0.2;
 PPm_box1 = PPm_box1 + [0 +0.295 -1.365];
+%Vector_temp = randn(size(PPm_box1,1),3)*1;
+%Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
+%PPm_box1 = PPm_box1 + Vector_temp;
+
 PPm_EOC = PP03 ;
 PPm_EOC(:,1) = PPm_EOC(:,1) * 0.09;
 PPm_EOC(:,2) = PPm_EOC(:,2) * 0.09;
 PPm_EOC(:,3) = PPm_EOC(:,3) * 0.05;
 PPm_EOC = PPm_EOC + [-0.115 +0.295 -1.615];
+%Vector_temp = randn(size(PPm_EOC,1),3)*1;
+%Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
+%PPm_EOC = PPm_EOC + Vector_temp;
 
 PPm_box2 = PP04 ;
 PPm_box2(:,1) = PPm_box2(:,1) * 0.05;
 PPm_box2(:,2) = PPm_box2(:,2) * 0.2;
 PPm_box2(:,3) = PPm_box2(:,3) * 0.1;
 PPm_box2 = PPm_box2 + [0.2 -0.215 -1.265];
+%Vector_temp = randn(size(PPm_box2,1),3)*1;
+%Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
+%PPm_box2 = PPm_box2 + Vector_temp;
+
 PPm_box3 = PP04 ;
 PPm_box3(:,1) = PPm_box3(:,1) * 0.05;
 PPm_box3(:,2) = PPm_box3(:,2) * 0.2;
 PPm_box3(:,3) = PPm_box3(:,3) * 0.2;
 PPm_box3 = PPm_box3 + [-0.2 -0.215 -1.365];
+%Vector_temp = randn(size(PPm_box3,1),3)*1;
+%Vector_temp = Vector_temp ./ vecnorm(Vector_temp,2,2) * 0.2;
+%PPm_box3 = PPm_box3 + Vector_temp;
 
 %PPmm = PPmm + shiftReal;
-PPm_total=[PPm_body ; PPm_panel1; PPm_panel2;PPm_box1;PPm_EOC;PPm_box2;PPm_box3];
+%PPm_total=[PPm_body ; PPm_panel1; PPm_panel2;PPm_box1;PPm_EOC;PPm_box2;PPm_box3];
+PPm_total=[PPm_body ; PPm_box1;PPm_EOC;PPm_box2;PPm_box3];
 
 q= [1 0 0 0];
 %q= [cos(25/57.92) 0 sin(25/57.92) 0];
@@ -67,6 +78,7 @@ rotm= quat2rotm(q);
 PPmR_body = PPm_body * rotm.';
 
 
+%{
 figure
 h = PPm_total(:,3);
 scatter3(PPm_total(:,1),PPm_total(:,2),PPm_total(:,3),1,h,'filled');
@@ -81,50 +93,15 @@ xlim([-2 2]);
 ylim([-4 4]);
 zlim([-2 2]);
 %title('Pointcloud of KOMPSAT-1 Model')
-
-%% 원래 가지고 있던 파일로 개형 만들기
-
-
-E1=load("equation_body_margin.mat");
-E2=load("equation_panel1_margin.mat");
-E3=load("equation_panel2_margin.mat");
-[coeffs1, terms1]=coeffs(E1.f2_translated_expanded, [x y z]);
-[coeffs2, terms2]=coeffs(E2.f2_translated_expanded, [x y z]);
-[coeffs3, terms3]=coeffs(E3.f2_translated_expanded, [x y z]);
-
-figure
-hold on
-fimplicit3(E1.f2_translated_expanded,[-3 3 -4 4 -2 2],'FaceColor', [0.99 0.75 0.12]);
-fimplicit3(E2.f2_translated_expanded,[-2 2 -4 4 -2 2],'FaceColor', [0.05, 0.2, 0.5]);
-fimplicit3(E3.f2_translated_expanded,[-2 2 -4 4 -2 2],'FaceColor', [0.05, 0.2, 0.5]);
-hold off
-
-
-colormap(jet);
-xlabel ('X (m)')
-ylabel ('Y (m)')
-zlabel ('Z (m)')
-axis equal
-grid on
-view([1 1 1]);
-xlim([-1 1]);  
-ylim([-4 4]);
-zlim([-2 2]);
-
-figure
-hold on
-scatter3(PPm_body(:,1),PPm_body(:,2),PPm_body(:,3),3,PPm_body(:,3),'filled');
-scatter3(PPm_panel1(:,1),PPm_panel1(:,2),PPm_panel1(:,3),1,PPm_panel1(:,3),'filled');
-scatter3(PPm_panel2(:,1),PPm_panel2(:,2),PPm_panel2(:,3),1,PPm_panel2(:,3),'filled');
-hold off
+%}
 
 %% 
 syms x;
 syms y;
 syms z;
 order = 6;
-PPm_use = PPm_panel2;
-
+PPm_use = PP01;
+varName='01';
 %% 비동차항 먼저
 %{
 
@@ -163,11 +140,11 @@ title('Initial')
 %}
 %% 그냥 좌표 평균 찾기 겸 점 초기세팅
 
-center_shift = median(PPm_use,1);
+center_shift = median(PPm_use,1)
 PPm_shift = PPm_use - center_shift;
 
-shiftResidue = shiftReal.'
-shiftResidue = shiftResidue - center_shift.'
+shiftResidue = shiftReal.';
+shiftResidue = shiftResidue - center_shift.';
 
 %% Iteration 초기 설정
 %shifted_f = subs(poly, [x, y, z], [x - a, y - b, z - c])
@@ -207,6 +184,7 @@ f2_handle = matlabFunction(f2_translated_expanded, 'Vars', [x,y,z]);
 [coeffs1, monomial] = coeffs(f2_partial , [x,y,z]);
 %coeffs(f2_substituted , [x,y,z])
 
+%{
 figure
 h2 = scatter3(PPm_shift(:,1),PPm_shift(:,2),PPm_shift(:,3),1,PPm_shift(:,3),'filled');
 colormap(jet);
@@ -221,12 +199,13 @@ view([1, 1, 1]);
 axis equal
 %title('Pointcloud Model - Panel')
 %title('Pointcloud Model - Body')
-
+%}
+%{
 figure
 h2 = scatter3(PPm_use(:,1),PPm_use(:,2),PPm_use(:,3),1,PPm_use(:,3),'filled');
 colormap(jet);
 hold on
-h1 = fimplicit3(f2_translated_expanded,[-6 6 -6 6 -6 6],'FaceColor', [0.05, 0.2, 0.5]);
+h1 = fimplicit3(f2_translated_expanded,[-6 6 -6 6 -6 6],'FaceColor', [0.99 0.75 0.12]);
 hold off
 
 xlabel ('X (m)')
@@ -235,14 +214,21 @@ zlabel ('Z (m)')
 view([1, 1, 1]);
 axis equal
 %title('Pointcloud Model - Panel')
+%}
+
 %{
 figure
-h2 = scatter3(PPm_shift(:,1),PPm_shift(:,2),PPm_shift(:,3),1,PPm_shift(:,3),'filled');
-colormap(jet);
-hold on
-f2_ngd = @(x,y,z) 1.10736*x^6 + 2.46766*y^6+ 0.0163682*x^5*y + 12.3976*x^4*y^2 - 0.434712*x^3*y^3 - 2.58541*x^2*y^4  + 0.143499*x*y^5 - 0.286339*x^4*y*z  - 0.0736799*x*y^4*z - 0.0199605*y^5*z - 0.11549*x^4*z^2 - 0.254459*x^2*y^2*z^2  - 0.210524*y^4*z^2 - 0.093437*x^2*z^4 + 0.016984*x*y*z^4 - 0.0840934*y^2*z^4  + 0.0635403*z^6-1;
-h1 = fimplicit3(f2_ngd,[-6 6 -6 6 -6 6],'FaceColor', [0.99 0.75 0.12]);
-hold off
+
+f2_ngd = @(x,y,z)  5*( x^4*(y^2+z^2) +y^4*(z^2+x^2)+z^4*(x^2+y^2))-1;
+f2_ngd = @(x,y,z)  x^2*y^2*z^2-1;
+f2_ngd = @(x,y,z)  x^6+y^6+z^6 - 0.40*( x^4*(y^2+z^2) +y^4*(z^2+x^2)+z^4*(x^2+y^2))+0.9*x^2*y^2*z^2-1;
+fimplicit3(f2_ngd,[-6 6 -6 6 -6 6],'FaceColor', [0.99 0.75 0.12]);
+
+
+f2_ngd = @(x,y,z)  x^6 - 15*x^4*y^2+15*x^2*y^4 - y^6-1;
+
+f2_ngd = @(x,y,z)  1.35*(x^2+y^2)^3+z^6 - 0.35*( x^6 - 15*x^4*y^2+15*x^2*y^4 - y^6)-1;
+fimplicit3(f2_ngd,[-6 6 -6 6 -6 6],'FaceColor', [0.99 0.75 0.12]);
 xlabel ('X (m)')
 ylabel ('Y (m)')
 zlabel ('Z (m)')
@@ -337,10 +323,18 @@ zlabel ('Z (m)')
 view([1, 1, 1]);
 axis equal
 
-%{
-norm(error_shift,1)
-evaluateModel(PPm_shift,f2_numeric, beta_values)
+%함수 평가 
+equation2string(beta_values, TermsB);
+sum(beta_values);
 
+f2_handle(0,+0.866,0)+1
+f2_handle(1,0,0)+1
+f2_handle(-0.5,-0.866,0)+1
+f2_handle(-0.5,-0.866,1)+1
+%norm(error_shift,1)
+%evaluateModel(PPm_shift,f2_numeric, beta_values)
+
+%{
 figure
 subplot(1,3,1)
 plot(shiftResidueSet(1,:))
@@ -348,4 +342,50 @@ subplot(1,3,2)
 plot(shiftResidueSet(2,:))
 subplot(1,3,3)
 plot(shiftResidueSet(3,:))
+%}
+%%
+%{
+threshold = prctile(error_shift, 10); % 하위 15%에 해당하는 값 (15th percentile)
+
+% 2. 조건을 만족하는 인덱스 찾기
+selectedIdx = find(error_shift <= threshold); % 하위 15% 선택
+
+% 3. 새로운 PPm_out 생성
+PPm_out = PPm_shift(selectedIdx, :); % 조건을 만족하는 점만 선택
+[beta_values,error]    = regressionFourthOrder(PPm_out,TermsB);
+f2_partial = @(x,y,z) f2_numeric([x,y,z], beta_values.')-1;
+
+f3_substituted = subs(f2, betaB, beta_values.');
+f3_translated = subs(f3_substituted-1, [x,y,z], [x-shiftSum(1), y-shiftSum(2), z-shiftSum(3)]);
+f3_translated_expanded = expand(f3_translated);
+
+%{
+figure
+h2 = scatter3(PPm_out(:,1),PPm_out(:,2),PPm_out(:,3),1,PPm_out(:,3),'filled');
+colormap(jet);
+hold on
+h1 = fimplicit3(f2_partial,[-6 6 -6 6 -6 6],'FaceColor', [0.99 0.75 0.12]);
+hold off
+
+xlabel ('X (m)')
+ylabel ('Y (m)')
+zlabel ('Z (m)')
+view([1, 1, 1]);
+axis equal
+%}
+%{
+figure
+h2 = scatter3(PPm_use(:,1),PPm_use(:,2),PPm_use(:,3),1,PPm_use(:,3),'filled');
+colormap(jet);
+hold on
+h1 = fimplicit3(f3_translated_expanded,[-6 6 -6 6 -6 6],'FaceColor', [0.99 0.75 0.12]);
+hold off
+
+xlabel ('X (m)')
+ylabel ('Y (m)')
+zlabel ('Z (m)')
+view([1, 1, 1]);
+axis equal
+%}
+%save(['equation_' varName '_margin.mat'], 'f3_translated_expanded');
 %}
--- testcode2.m
+++ testcode2.m
@@ -1,22 +1,22 @@
-%2024 08 20
-PP02 =generateRandomPointsOnCylinder(6000)+randn(6000,3)*0.0005;
+%2025 1 14
+PP02 =generateRandomPointsOnCylinder(6000)+randn(6000,3)*0.01;
 
-shiftReal =  [0.20 -0.1 -0.1];
+shiftReal =  [1.20 -3.5 -3.1];
 PPm = PP02 + shiftReal;
 PPmm= PP02;
 PPmm(:,3) = PPmm(:,3) * 2;
 PPmm = PPmm + shiftReal;
 
 
-PP2=PP02(PP02(:,3)>0.5,:);
-PP6=PP02(PP02(:,2)>0.652&PP02(:,3)<0&PP02(:,1)>0,:);
-PP7=PP02(PP02(:,2)*1.7+PP02(:,3)>0.5,:);
+PP2=PP02(PP02(:,2)>0.5,:);
+PP6=PP02(PP02(:,3)<0&PP02(:,1)>0,:);
+PP7=PP02(PP02(:,2)*1.7+PP02(:,3)>-0.2,:);
 PPm2= [PP2] + shiftReal;
 PPm26= [PP2;PP6] + shiftReal;
+PPm7= [PP7] + shiftReal;
 
 
-
-%%
+%% 비동차항 먼저 추정
 syms x;
 syms y;
 syms z;
@@ -25,27 +25,35 @@ TermsA = nonhomogeneTerm(order);
 betaA = sym('beta', [1, length(TermsA)]);
 f1 = sum(betaA .* TermsA);
 
-PPm_use = PPm;
+PPm_use = PPm7;
 [beta_values0,error0]    = regressionFourthOrder(PPm_use,TermsA);
 f1_numeric = matlabFunction(f1, 'Vars', {[x, y, z], betaA});
 f1_partial = @(x,y,z) f1_numeric([x,y,z], beta_values0.')-1;
 
 PPm_shift = PPm_use;  % 초기 PPm 설정
-shiftResidue = shiftReal.';
+
+%{
+figure
+hold on
+fimplicit3(f1_partial,[-4.5 4.5 -4.5 4.5 -4.5 4.5]);
+scatter3(PPm_use(:,1),PPm_use(:,2),PPm_use(:,3),2,'r','filled');
+hold off
+axis equal
+title('Initial')
+%}
+%% 비동차항을 동차항으로 추정
 %f1_total   = subs(f1,betaA,beta_values0);
 %syms a b c 
 %f1_t_shift = subs(f1_total, [x, y, z], [x-a, y-b, z-c])
 
-figure
-hold on
-fimplicit3(f1_partial,[-4.5 4.5 -4.5 4.5 -4.5 4.5]);
-scatter3(PPm_use(:,1),PPm_use(:,2),PPm_use(:,3),'r');
-hold off
-axis equal
-title('Initial')
 
-% 초기 값 설정
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+%% 그냥 좌표 평균 찾기 겸 점 초기세팅
+
+center_shift = median(PPm_use,1);
+PPm_shift = PPm_use - center_shift;
+
+shiftResidue = shiftReal.'
+shiftResidue = shiftResidue - center_shift.'
 %%
 TermsB = homogeneTerm(order);
 betaB = sym('beta', [1, length(TermsB)]);
@@ -59,28 +67,29 @@ d1_numeric = matlabFunction(difx, 'Vars', {[x, y, z], betaB});
 d2_numeric = matlabFunction(dify, 'Vars', {[x, y, z], betaB});
 d3_numeric = matlabFunction(difz, 'Vars', {[x, y, z], betaB});
 
-[beta_values,error]    = regressionFourthOrder(PPm_use,TermsB);
+[beta_values,error]    = regressionFourthOrder(PPm_shift,TermsB);
 
 error_shift = error;  % 초기 에러 설정
 
+shiftSet = [];
+shift1Set = [];
 shiftResidueSet = [];
-numIterations = 500;  % 반복 횟수 설정
-
-
+numIterations = 40;  % 반복 횟수 설정
 
 f2_partial = @(x,y,z) f2_numeric([x,y,z], beta_values.')-1;
-%%f2_partialgraph = @(x,y,z) f2_numeric([x,y,z], beta_values.');
-
-
-
+%{
 figure
 hold on
 fimplicit3(f2_partial,[-4.5 4.5 -4.5 4.5 -4.5 4.5]);
-scatter3(PPm_use(:,1),PPm_use(:,2),PPm_use(:,3),'r');
+scatter3(PPm_shift(:,1),PPm_shift(:,2),PPm_shift(:,3),2,'k','filled');
 hold off
 axis equal
-title('Before')
+view([1,1,1]);
+%}
+
 %%
+shift_0= [];
+shift_1= [0;0;0];
 for i = 1:numIterations
     % 미분함수에 정의
     dx = d1_numeric(PPm_shift, beta_values.');
@@ -89,12 +98,15 @@ for i = 1:numIterations
     dxyz = [dx dy dz];
 
     [shift, residual] = regressionShift(PPm_shift, error_shift,dxyz);
-    shiftResidue = shiftResidue + shift;
+    shift_1 = shift*0.85 + shift_1*0.5;
+    shiftSet = [shiftSet shift];
+    shift1Set = [shift1Set shift_1];
+    shiftResidue = shiftResidue + shift_1;% 부호가 반대인데 알아서 해석하세요
     shiftResidueSet = [shiftResidueSet shiftResidue];
     
-    PPm_shift = PPm_shift + shift.';
+    PPm_shift = PPm_shift + shift_1.'; %따라서 여기도 부호가 반대임
     [beta_values, error_shift] = regressionFourthOrder(PPm_shift,TermsB);
-
+    
     %0.001s 
     
 end
@@ -103,17 +115,17 @@ f2_partial = @(x,y,z) f2_numeric([x,y,z], beta_values.')-1;
 figure
 hold on
 fimplicit3(f2_partial,[-4.5 4.5 -4.5 4.5 -4.5 4.5]);
-scatter3(PPm_shift(:,1),PPm_shift(:,2),PPm_shift(:,3),'black');
+scatter3(PPm_shift(:,1),PPm_shift(:,2),PPm_shift(:,3),2,'k','filled');
 %scatter3(PP02(:,1),PP02(:,2),PP02(:,3),'b');
 hold off
 axis equal
-xlim([-4 4]);
-ylim([-4 4]);
-title('Aftor')
+view([1,1,1]);
+
+title('After')
 
 
-norm(error_shift,1)
-evaluateModel(PPm_shift,f2_numeric, beta_values)
+%norm(error_shift,1)
+%evaluateModel(PPm_shift,f2_numeric, beta_values)
 
 figure
 subplot(1,3,1)
@@ -122,3 +134,21 @@ subplot(1,3,2)
 plot(shiftResidueSet(2,:))
 subplot(1,3,3)
 plot(shiftResidueSet(3,:))
+
+%shift 추이
+figure
+subplot(1,3,1)
+hold on
+plot(shiftSet(1,:))
+plot(shift1Set(1,:))
+hold off
+subplot(1,3,2)
+hold on
+plot(shiftSet(2,:))
+plot(shift1Set(2,:))
+hold off
+subplot(1,3,3)
+hold on
+plot(shiftSet(3,:))
+plot(shift1Set(3,:))
+hold off
